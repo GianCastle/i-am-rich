8399d88d365e1ddc271b30da83cae6a519ccfe35
Giancarlos Castillo <gc.c@outlook.com>
2020-03-02 17:30:58 -0400

    feat: Flutter - I Am Rich App

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(AppWidget());
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[800],
+          title: Text('I Am Rich'),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
